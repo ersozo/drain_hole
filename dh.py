27227117fb82644e63123f8478f8888257462362
ozgur <ozgur.ersoz3@gmail.com>
2019-11-23 14:35:31 +0300

    edge detection uygulandı, limit için scale seçildi

--- dh.py
+++ dh.py
@@ -7,6 +7,8 @@ import time
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 1000   # Set Duration To 1000 ms == 1 second
 
+scale = 1.1
+
 x1, x2 = 250, 400
 y1, y2 = 100, 250
 
@@ -19,33 +21,35 @@ font = cv2.FONT_HERSHEY_COMPLEX
 cap = cv2.VideoCapture(1)
 
 # capturing frames to adapt to ambience lighting.
-for i in range(1,6):
+for i in range(1, 6):
     ret, frame = cap.read()
 
 grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 maskedGrayFrame = cv2.bitwise_and(grayFrame, grayFrame, mask=mask)
-limit = maskedGrayFrame.mean()
-print("{:.2f}".format(limit))
-
+blurred_img = cv2.blur(maskedGrayFrame, ksize=(5, 5))
+edges = cv2.Canny(blurred_img, threshold1=127, threshold2=127)
+limit = edges.mean() * scale   # for comparison value
 
 while True:
     ret, frame = cap.read()
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     maskedGrayFrame = cv2.bitwise_and(grayFrame, grayFrame, mask=mask)
-   
-    value = maskedGrayFrame.mean()
+    blurred_img = cv2.blur(maskedGrayFrame, ksize=(5, 5))
+    edges = cv2.Canny(blurred_img, threshold1=127, threshold2=127)
+    value = edges.mean()
 
     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-    if value < limit:
+    if value > limit:
+
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, text_result, (290, 300), font, 1.5, (0, 255, 0), 2)
         cv2.putText(frame, str(value)[:4], (290, 340), font, 1, (0, 255, 0), 2)
-        
+
         winsound.Beep(frequency, duration)
 
     cv2.putText(frame, text_control, (50, 450), font, 1, (255, 0, 0), 2)
-    # cv2.putText(frame, str(limit)[:4], (5, 30), font, 1, (0, 0, 255), 2)
+    cv2.putText(frame, str(limit)[:4], (5, 30), font, 1, (0, 0, 255), 2)
 
     cv2.imshow("control", frame)
 
